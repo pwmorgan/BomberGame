4cdd48a3ccadf9b32bb005ac2b182f8dbe6c7d79
Patrick Morgan <p.w.morgan@gmail.com>
2013-09-21 15:16:56 -0700

    Added explosion.

---
+++ BomberGame/Assets/Scripts/ExplosionController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionController : MonoBehaviour {
+	
+	public int explosionRate;
+	public int maxScale;
+	
+	private float _scale = 1;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_scale += explosionRate * Time.deltaTime;
+		transform.localScale = new Vector3(_scale, _scale, _scale);
+		
+		if (_scale > maxScale) {
+			Destroy(gameObject);
+		}
+	}
+}
--- BomberGame/Assets/Scripts/PlayerController.cs
+++ BomberGame/Assets/Scripts/PlayerController.cs
@@ -4,33 +4,60 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	
 	public int speed;
+	public Transform explosion;
 	
+	private enum State {
+		ALIVE,
+		DEAD
+	}
 	private Vector3 _velocity;
 	private CharacterController _controller;
+	private State _state;
+	
 	
 	// Use this for initialization
 	void Start () {
 		_controller = GetComponent<CharacterController>();
+		_state = State.ALIVE;
 	}
 	
 	// Update is called once per frame
 	
 	void Update () {
-		// Get input
-		float xVel = Input.GetAxis("Horizontal");
-		float yVel = Input.GetAxis("Vertical");
 		
-		// Horizontal Movement
-		_velocity.x = xVel * speed * Time.fixedDeltaTime;
-		
-		// Vertical Movement
-		_velocity.z = yVel * speed * Time.fixedDeltaTime;
-		
-		// Update position
-		_controller.Move(_velocity);
-		
-		// Space Bar Explodes
-			// Tell Game manager an explosion is starting.
-			// Create an explosion at player location.
+		switch (_state) {
+			case State.ALIVE:
+				// Get input
+				float xVel = Input.GetAxis("Horizontal");
+				float yVel = Input.GetAxis("Vertical");
+				
+				// Horizontal Movement
+				_velocity.x = xVel * speed * Time.fixedDeltaTime;
+				
+				// Vertical Movement
+				_velocity.z = yVel * speed * Time.fixedDeltaTime;
+				
+				// Update position
+				_controller.Move(_velocity);
+				
+				// Create Explosion
+				if (Input.GetButtonDown("Detonate")) {
+					// Create an explosion at player location.
+		            Instantiate(explosion, transform.position, transform.rotation);
+					_state = State.DEAD;
+				}
+			
+				break;
+			
+			case State.DEAD:
+			
+				break;
+			
+			default:
+			
+				// Something is amiss
+				break;
+			
+		}
 	}
 }
