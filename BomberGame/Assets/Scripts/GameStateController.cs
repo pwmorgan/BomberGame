6ed7369eef2581b0d57ebfb35dd12b253300844a
Ashley's 2007 MBP <oldmacbookpro@ashley-mas-computer-2.local>
2013-09-21 14:26:54 -0700

    Game State manager time scale / check enemies implemented

--- BomberGame/Assets/Scripts/GameStateController.cs
+++ BomberGame/Assets/Scripts/GameStateController.cs
@@ -6,8 +6,14 @@ public class GameStateController : MonoBehaviour {
 	//
 	public string[] EnemyTags = new string[1];
 	
-	//
+	// Enemies game object array
 	private GameObject[] Enemies = new GameObject[0];
+	
+	// Time tracking
+	private float SlowTimeStart;
+	private float SlowTimeDuration;
+	private float SlowTimeElapsed = 0.0f;
+	private bool IsTimeSlowed = false;
 	
 	
 	// Use this for initialization
@@ -18,49 +24,69 @@ public class GameStateController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		// Mouse click, for debuggin only
 		/*
 		if (Input.GetMouseButtonDown(0)) {
-			CheckEnemies();	
+			SlowTime( );	
+		}*/
+		
+		// is time slowed down?
+		// this is for bullet time duration
+		if (IsTimeSlowed) {
+			SlowTimeElapsed = Time.time - SlowTimeStart;
+			
+			Debug.Log( "SlowTimeElapsed: " + SlowTimeElapsed );
+			if (SlowTimeElapsed >= SlowTimeDuration) {
+				ResumeTime();
+			}
 		}
-		*/
+		
 	}
 	
 	// Method: CheckEnemies, sees who is alive or dead in the scene
 	public void CheckEnemies() {
 		Debug.Log( "CheckEnemies" );
 		
-		// loop through scene, identify enemies
-		GameObject[] enemyObjects;
-		enemyObjects = GameObject.FindGameObjectsWithTag("Enemy");
-		Debug.Log( "Total Enemies: " + enemyObjects.Length );
+		// identify enemies
+		Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		Debug.Log( "Total Enemies: " + Enemies.Length );
 		
-		/*
-		// loop through and kill, to test
-		for ( int i=0; i<enemyObjects.Length; i++) {
-			Debug.Log( i );
-			EnemyController ECScript =  enemyObjects[ i ].GetComponent<EnemyController>();
+		// kill applicable enemies
+		foreach (GameObject enemy in Enemies) {
+			EnemyController ECScript = enemy.GetComponent<EnemyController>();
 			
+			// for now, kill the enemy
+			// TODO: logic for whether or not kill the enemy 
+			ECScript.Kill();
 		}
-		*/
-		
 		
 	}
 	
 	// Method: SlowTime, slows timescale for explosion
-	public void SlowTime( float t=0.1f ) {
+	public void SlowTime( float dur=0.2f, float min=0.1f  ) {
 		Debug.Log( "SlowTime" );
 		
 		// assign time scale
 		// TODO: Tween this value
-		Time.timeScale = t;
+		Time.timeScale = min;
+		
+		// Track duration
+		SlowTimeStart = Time.time;
+		SlowTimeDuration = dur;
+		IsTimeSlowed = true;
+		
 	}
 	
 	// Method: ResumeTime, speeds time back up to default
-	public void ResumeTime( ) {
+	private void ResumeTime( ) {
 		Debug.Log( "ResumeTime" );
 		
 		// assign time scale
 		// TODO: Tween this value
 		Time.timeScale = 1.0f;
+		
+		// Tracking
+		IsTimeSlowed = false;
 	}
 }
