16610f02adb5c63fbe570e8258e8963341720417
Patrick Morgan <p.w.morgan@gmail.com>
2013-09-21 13:33:31 -0700

    Basic player controller.

---
+++ BomberGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public int speed;
+	
+	private Vector3 _velocity;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	
+	void Update () {
+		// Horizontal Movement
+		float xVel = Input.GetAxis("Horizontal");
+		float yVel = Input.GetAxis("Vertical");
+		
+		_velocity.x = xVel * speed * Time.fixedDeltaTime;
+		
+		// Vertical Movement
+		_velocity.z = yVel * speed * Time.fixedDeltaTime;
+		
+		
+		// Update position
+		transform.Translate(_velocity);
+		
+		// Space Bar Explodes
+			// Tell Game manager an explosion is starting.
+			// Create an explosion at player location.
+	}
+}
