a1dd095deea6e83d6f99bcc23835f2c65aaa6e9e
Ashley's 2007 MBP <oldmacbookpro@ashley-mas-computer-2.local>
2013-09-22 10:50:29 -0700

    sound explosion prefab support

---
+++ BomberGame/Assets/Scripts/ExplosionAudioController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionAudioController : MonoBehaviour {
+	
+	public AudioClip[] ExplosionSounds = new AudioClip[0];
+	
+	// Use this for initialization
+	void Start () {
+		
+		AudioClip ExplosionSound = ExplosionSounds[ Random.Range( 0, ExplosionSounds.Length ) ];
+		audio.clip = ExplosionSound;
+		
+		audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
