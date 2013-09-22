7b00b0d40a0c2ebbfcff262cad0ad57a2ba410a7
Ashley's 2007 MBP <oldmacbookpro@ashley-mas-computer-2.local>
2013-09-22 13:16:07 -0700

    adding blood splatter prefab

---
+++ BomberGame/Assets/Scripts/SplatterController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplatterController : MonoBehaviour {
+	
+	public Material[] SplatterMaterials = new Material[0];
+	
+	// Use this for initialization
+	void Start () {
+		Material splatter = SplatterMaterials[ Random.Range( 0, SplatterMaterials.Length ) ];
+		renderer.material = splatter;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
