ee1e15b3ff4b3fc57ee1509479b0070c46c8af47
Ashley's 2007 MBP <oldmacbookpro@ashley-mas-computer-2.local>
2013-09-22 11:00:48 -0700

    adding blood hit sfx (not supported yet) and shockwave fade controls

---
+++ BomberGame/Assets/Scripts/ShockwaveController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShockwaveController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		iTween.FadeTo( gameObject, iTween.Hash( "alpha", 0.2f, "time", 0.2f, "easetype", iTween.EaseType.easeInCubic ) );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
